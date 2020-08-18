fff425d02348c4d445cb8020a9bb80332a1a3386
Daniel Araya Sambucci <danielarayasambucci@gmail.com>
2020-08-17 18:55:26 -0600

    Configuracion

--- Configuracion.py
+++ Configuracion.py
@@ -3,7 +3,6 @@ ALTO = 600
 
 BLANCO = (255, 255, 255)
 NEGRO = (0,0,0)
-<<<<<<< HEAD
 LINO = (250,240,230)
 CELESTE = (96, 216, 232)
 CELDABLOQUEADA = (189,189,189)
@@ -16,21 +15,6 @@ AMARILLO = (229,190,1)
 CAFE = (128,64,0)
 TURQUESA = (93,193,185)
 TURQ_OSCURO = (0,32,32)
-=======
-ROSADO = (255,0,255)
-AZUL = (0,0,255)
-VERDE = (0,255,0)
-ROJO = (255,0,0)
-MORADO = (180,26,236)
-NARANJA = (255,128,0) 
-CAFE = (153, 76, 0)
-AMARILLO = (255,255,0)
-CELESTE = (96, 216, 232)
-CELDABLOQUEADA = (189,189,189)
-COLORINCORRECTO = (195,121,121)
-
-COLORES = [AZUL, VERDE, ROJO, MORADO, NARANJA, CAFE, AMARILLO, CELESTE, ROSADO]
->>>>>>> c9d91527c43224e5a9b1808b371382df475e4cb6
 resuelto = [[[5,3,4],[6,7,8],[9,1,2]], #resuelto[x]= fila
             [[6,7,2],[1,9,5],[3,4,8]], #resuelto[x][x]= fila, columna
             [[1,9,8],[3,4,2],[5,6,7]], #resuelto[x][x][x]= fila, columna, espacio
